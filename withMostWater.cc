70adf1a61ef79362e5075ab4013d43ea9d1c5f8d
pingpan <pingpan@umich.edu>
2014-09-30 14:08:41 -0400

    container with most water

---
+++ withMostWater.cc
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *           Desc:  container with most water
+ *        Created:  09/20/2014 05:36:08 PM
+ *       Compiler:  g++ 4.7.0
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+public:    
+    int withMostWater(unsigned a[]){
+        int l = 0;
+        int h = n-1;
+        
+        int tempMax = INT_MIN; 
+            
+        while(l <= h){
+            tempVol = (a[0] + a[n-1])*(n-1);
+
+        }
+    }
+};
+
+int main(){
+
+}
+
+
+
+
