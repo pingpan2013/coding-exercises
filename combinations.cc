7e98250b0c4afefa057b7f0df71a48fed65909e0
pingpan <pingpan@umich.edu>
2014-10-01 21:18:12 -0400

    combinations

---
+++ combinations.cc
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *    Description:  Given two integers n and k, find out all the combinations
+ *                  of k numbers out of 1...n
+ *       Compiler:  g++ 4.7.0
+ * =====================================================================================
+ */
+
+
+#include "tree.h"
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+public:    
+    vector<vector<int> > allCombinations(int n, int k){
+        vector<vector<int> > result;
+    }
+
+private:
+
+};
+
+int main(){
+    Solution sol;
+
+}
+
+
+
