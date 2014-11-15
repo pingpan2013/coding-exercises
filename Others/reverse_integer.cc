44feefe83ccec9ab91b9bbb7814dd3eb8e1f921b
pingpan <pingpan@umich.edu>
2014-11-15 18:02:31 -0500

    didn't handle overflow problems well

--- Others/reverse_integer.cc
+++ Others/reverse_integer.cc
@@ -14,37 +14,43 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <cassert>
 
 using namespace std;
 
 class Solution{
 public:
-    void reverseInteger(int n){
-        vector<int> result;
-        bool flag = false;
-
-        while(n != 0){
-            if(!flag){
-                result.push_back(n%10);
-                flag = true;
-            }
-            else    
-                result.push_back(abs(n%10));
-            n /= 10;
+    // without extra space
+    int reverse(int x){
+        int r;
+        int res = 0;
+        while(x != 0){
+            r = x % 10;
+            res = res*10 + r;
+            x /= 10;
         }
         
-        for(auto i: result)
-            cout << i;
-        cout << endl;
+        // deal with overflow
+        if((x < 0 && res > 0) ||
+                (x > 0 && res < 0))    return 0;
+
+        return res;
     }
 };
 
 int main(){
     Solution sol;
-    sol.reverseInteger(2345);
-    sol.reverseInteger(1);
-    sol.reverseInteger(-12345);
+    assert(sol.reverse(1) == 1 && "failed in test 1");
+    assert(sol.reverse(12) == 21 && "failed in test 2");
+    assert(sol.reverse(123) == 321 && "failed in test 3");
+    assert(sol.reverse(-1234) == -4321 && "failed in test 4");
+    
+    // testcases for the overflow problem
+    assert(sol.reverse(2147483647) == 0 && "failed in test 5");
 
+    cout << "Passed all the tests!" << endl;
+
+    return 0;
 }
 
 
