ae6e571a0a877679660c7ab4d0532e246b055d82
pingpan <pingpan@umich.edu>
2015-02-01 18:17:11 -0500

    quicksort

---
+++ Sort and Search/quicksort.cc
@@ -0,0 +1,74 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  quicksort.cc
+ *
+ *    Description:  implement quicksort with random pivot
+ *
+ *        Created:  02/01/2015 17:18:03
+ *       Compiler:  g++ 4.7.0
+ *
+ * =====================================================================================
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    void quicksort(vector<int> vec){
+        printVec(vec);
+        quickSort(vec, 0, vec.size()-1);
+        printVec(vec);
+    }
+    
+private:
+    void printVec(vector<int> &vec){
+        for(auto itr : vec)
+            cout << itr << " ";
+        cout << endl;
+    }
+
+
+    void quickSort(vector<int> &arr, int left, int right) {
+        if(left >= right)   return;
+        
+        int i = left, j = right;
+        int pivot = arr[(left + right) / 2];
+ 
+        /* partition */
+        while (i < j) {
+            while (arr[i] < pivot)
+                  i++;
+            while (arr[j] > pivot)
+                  j--;
+            if (i <= j) {   // attention  <=, guranteeed that j > i in the final
+                  swap(arr[i++], arr[j--]);
+            }
+        };
+ 
+        /* recursion */
+        quickSort(arr, left, j);
+        quickSort(arr, i, right);
+    
+        printVec(arr);
+    }
+};
+
+
+int main(){
+    //vector<int> vec({1, 2, 4, 5, 1, 3, 10, 20, 9, 12, 13, 0}); 
+    
+    vector<int> vec({5, 4,3,2,1});
+
+    
+    Solution sln;
+    sln.quicksort(vec);
+
+    return 0;
+}
+
+
