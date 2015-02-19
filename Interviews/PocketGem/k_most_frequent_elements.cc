37db6333885c53adb3649ca41d8d36c8388f229c
pingpan <pingpan@umich.edu>
2015-02-19 00:41:09 -0500

    k most frequent elements

--- Interviews/PocketGem/k_most_frequent_elements.cc
+++ Interviews/PocketGem/k_most_frequent_elements.cc
@@ -63,6 +63,35 @@ vector<int> findElements(vector<int> &vec, int k){
     return result;
 }
 
+/* =====================================================================
+ Follow up:
+
+ If the input stream is already sorted and offer you a iterator of the 
+ stream which has two functionalities:
+ hasNext();
+ next();
+
+ How would you solve this probelm?
+
+ int cnt = 0;
+ int tmp = next();
+ while(hasNext()){
+    if(tmp == next()){
+        cnt++;
+    }
+    else{
+        //  operate on the queue ... pop and push, maintain the size of k
+        // ...
+        
+        cnt = 0;
+        tmp = next();
+    }
+ }
+ 
+ * =====================================================================*/
+
+
+
 int main(){
     vector<int> vec({1, 1, 1, 2, 3, 3, 4, 4, 4, 5, 4, 6});
     vector<int> result = findElements(vec, 2);
