8a5eb90f92fc6f4ace9604a5ac62796a5e7a40e0
pingpan <pingpan@umich.edu>
2015-02-23 22:09:22 -0500

    deepest left leaf node

--- Interviews/Google/deepest_left_leaf_node.cc
+++ Interviews/Google/deepest_left_leaf_node.cc
@@ -16,8 +16,18 @@
 // max_level: the max_level of the tree
 // cur_level: current level of the tree when traversing
 // isLeft:    a flag to indicate if the current node is a left child
-void dfs(TreeNode *root, TreeNode *result, int &max_level, int cur_level, bool isLeft){
-       
+void dfs(TreeNode *root, TreeNode *&result, int &max_level, int cur_level, bool isLeft){
+    if(!root)   return;
+    
+    // current deepest left leaf node
+    if(cur_level > max_level && isLeft && !root->left && !root->right){
+        result = root;
+        max_level = cur_level;
+        return;
+    }
+
+    dfs(root->left, result, max_level, cur_level+1, true);
+    dfs(root->right, result, max_level, cur_level+1, false);
 }
 
 Node* deepestLeftLeafNode(TreeNode *root){
